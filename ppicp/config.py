b38a7bdfcf1f13565de82feb5e4fcd897560b4a1
bud-graziano <graziano.bud+github@gmail.com>
2016-08-09 21:52:25 +0200

    Update config file to save parameters for the fanmod software.
    The config now stores the default parameters used for the fanmod software.

--- ppicp/config.py
+++ ppicp/config.py
@@ -21,15 +21,48 @@ def write_config(out_path):
     config.add_section('io')
     config.set('io', 'num_pdb_download_threads', '2')
     config.set('io', 'num_dssp_download_threads', '5')
+    config.add_section('fanmod')
+    config.set('fanmod', '# = algorithm options', '')
+    config.set('fanmod', '# number of samples used to determine approx. # of subgraphs', '[100000]')
+    config.set('fanmod', 'num_samples_for_approx_num_subgraphs', '100000')
+    config.set('fanmod', '# full enumeration? 1(yes)/0(no)', '[1]')
+    config.set('fanmod', 'full_enumeration', '1')
+    config.set('fanmod', '# = input file', '')
+    config.set('fanmod', '# directed? 1(yes)/0(no)', '[1]')
+    config.set('fanmod', 'directed', '0')
+    config.set('fanmod', '# colored vertices? 1(yes)/0(no)', '[0]')
+    config.set('fanmod', 'colored_vertices', '0')
+    config.set('fanmod', '# colored edges? 1(yes)/0(no)', '[0]')
+    config.set('fanmod', 'colored_edges', '0')
+    config.set('fanmod', '# = random networks', '')
+    config.set('fanmod', '# random type: 0(no regard)/1(global const)/2(local const)', '[2]')
+    config.set('fanmod', 'random_type', '2')
+    config.set('fanmod', '# regard vertex colors? 1(yes)/0(no)', '[0]')
+    config.set('fanmod', 'regard_vertex_colors', '0')
+    config.set('fanmod', '# regard edge colors? 1(yes)/0(no)', '[0]')
+    config.set('fanmod', 'regard_edge_colors', '0')
+    config.set('fanmod', '# reestimate subgraph number? 1(yes)/0(no)', '[0]')
+    config.set('fanmod', 'reestimate_subgraph_num', '0')
+    config.set('fanmod', '# number of random networks', '[1000]')
+    config.set('fanmod', 'num_rand_networks', '1000')
+    config.set('fanmod', '# number of exchanges per edge', '[3]')
+    config.set('fanmod', 'num_exchanges_per_edge', '3')
+    config.set('fanmod', '# number of exchange attempts per edge', '[3]')
+    config.set('fanmod', 'num_exchange_attempts', '3')
+    config.set('fanmod', '# = output file', '')
+    config.set('fanmod', '# outfile format 1(ASCII - human readable)/0(CSV - for easy parsing)',
+               '[1]')
+    config.set('fanmod', 'outfile_format', '0')
+    config.set('fanmod', '# create dumpfile? 1(yes)/0(no)', '[0]')
+    config.set('fanmod', 'create_dumpfile', '1')
     config.add_section('external')
     config.set('external', 'calc_hydrogens', 'reduce')
-    config.set('external', 'calc_motifs', 'fanmod')
 
     with open(out_path, 'w') as f:
-        f.write('#These are the settings for PPICP\n#For further details on the values please '
-                'refer to the documentation.\n#Lines starting with # are comments and will '
-                'therefore be ignored.\n#{}\n\n'.format(datetime.datetime.now()
-                                                        .strftime('%a %b %Y %H:%M:%S')))
+        f.write('# These are the settings for the Protein-Protein Interaction Calculation Pipeline.'
+                '\n# For further details on the values please refer to the documentation.\n# Lines '
+                'starting with # are comments and will therefore be ignored.\n# {}\n\n'
+                .format(datetime.datetime.now().strftime('%a %b %Y %H:%M:%S')))
         config.write(f)
 
 
@@ -92,3 +125,158 @@ def get_motifs_app(conf_path):
     config = ConfigParser.SafeConfigParser()
     config.read(conf_path)
     return config.get('external', 'calc_motifs')
+
+
+def get_fanmod_num_samples(conf_path):
+    """
+    Get the number of samples that will be used to determine the approximate number of subgraphs.
+    :param conf_path: Path to the config file.
+    :return: the number of samples.
+    """
+    config = ConfigParser.SafeConfigParser()
+    config.read(conf_path)
+    return config.get('fanmod', 'num_samples_for_approx_num_subgraphs')
+
+
+def get_fanmod_enum(conf_path):
+    """
+    Check whether Fanmod should run a full enumeration to calculate the subgraphs or sampling.
+    :param conf_path: Path to the config file.
+    :return: 1 if full enumeration, 0 if sampling.
+    """
+    config = ConfigParser.SafeConfigParser()
+    config.read(conf_path)
+    return config.get('fanmod', 'full_enumeration')
+
+
+def get_fanmod_directed(conf_path):
+    """
+    Check whether the edges are treated as directed or undirected edges.
+    :param conf_path: Path to the config file.
+    :return: 1 if directed, 0 if undirected.
+    """
+    config = ConfigParser.SafeConfigParser()
+    config.read(conf_path)
+    return config.get('fanmod', 'directed')
+
+
+def get_fanmod_colored_vertices(conf_path):
+    """
+    Check whether the vertices are treated as colored vertices or not.
+    :param conf_path: Path to the config file.
+    :return: 1 if colored, 0 if not colored.
+    """
+    config = ConfigParser.SafeConfigParser()
+    config.read(conf_path)
+    return config.get('fanmod', 'colored_vertices')
+
+
+def get_fanmod_colored_edges(conf_path):
+    """
+    Check whether the edges are treated as colored edges or not.
+    :param conf_path: Path to config file.
+    :return: 1 if colored, 0 if not colored.
+    """
+    config = ConfigParser.SafeConfigParser()
+    config.read(conf_path)
+    return config.get('fanmod', 'colored_edges')
+
+
+def get_fanmod_random_type(conf_path):
+    """
+    Check which type of random networks should be generated.
+    :param conf_path: Path to the config file.
+    :return: 0 if no regard, 1 if global constant, or 2 if local constant.
+    """
+    config = ConfigParser.SafeConfigParser()
+    config.read(conf_path)
+    return config.get('fanmod', 'random_type')
+
+
+def get_fanmod_regard_vert_color(conf_path):
+    """
+    Check if the vertex colors should be considered during the generation of random networks.
+    :param conf_path: Path to the config file.
+    :return: 1 if vertex colors are considered, 0 if not.
+    """
+    config = ConfigParser.SafeConfigParser()
+    config.read(conf_path)
+    return config.get('fanmod', 'regard_vertex_colors')
+
+
+def get_fanmod_regard_edg_color(conf_path):
+    """
+    Check if the edge colors should be considered during the generation of random networks.
+    :param conf_path: Path to the config file.
+    :return: 1 if edge colors are considered, 0 if not.
+    """
+    config = ConfigParser.SafeConfigParser()
+    config.read(conf_path)
+    return config.get('fanmod', 'regard_edge_colors')
+
+
+def get_fanmod_reestimate(conf_path):
+    """
+    Check if the number of subgraphs should be re-estimated.
+    :param conf_path: Path to the config file.
+    :return: 1 if subgraphs should be re-estimated, 0 if not.
+    """
+    config = ConfigParser.SafeConfigParser()
+    config.read(conf_path)
+    return config.get('fanmod', 'reestimate_subgraph_num')
+
+
+def get_fanmod_num_rand_networks(conf_path):
+    """
+    Get the number of random networks that will be generated.
+    :param conf_path: Path to the config file.
+    :return: the number of random networks.
+    """
+    config = ConfigParser.SafeConfigParser()
+    config.read(conf_path)
+    return config.get('fanmod', 'num_rand_networks')
+
+
+def get_fanmod_num_exchanges_per_edge(conf_path):
+    """
+    Get the number of exchanges per edge for the random networks.
+    :param conf_path: Path to the config file.
+    :return: the number of exchanges per edge.
+    """
+    config = ConfigParser.SafeConfigParser()
+    config.read(conf_path)
+    return config.get('fanmod', 'num_exchanges_per_edge')
+
+
+def get_fanmod_num_exchange_attempts(conf_path):
+    """
+    Get the number of exchange attempts per edge for the random networks.
+    :param conf_path: Path to the config file.
+    :return: the number of exchange attempts per edge.
+    """
+    config = ConfigParser.SafeConfigParser()
+    config.read(conf_path)
+    return config.get('fanmod', 'num_exchange_attempts')
+
+
+def get_fanmod_outfile_format(conf_path):
+    """
+    Get the output file format. The options are ASCII format or CSV format.
+    :param conf_path: Path to the config file.
+    :return: 1 if ASCII output file format, 0 if CSV.
+    """
+    config = ConfigParser.SafeConfigParser()
+    config.read(conf_path)
+    return config.get('fanmod', 'outfile_format')
+
+
+def get_fanmod_dumpfile(conf_path):
+    """
+    Check if a dumpfile should be generated. The dumpfile will contain a list of all subnetworks
+    found in the input network including their adjacency matrix and the participating vertices.
+    :param conf_path: Path to the config file.
+    :return: 1 if dumpfile is created, 0 if not.
+    """
+    config = ConfigParser.SafeConfigParser()
+    config.read(conf_path)
+    return config.get('fanmod', 'create_dumpfile')
